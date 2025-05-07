d377f5cfb594f0a53c33dda20a11af149f4abdbf
Uacias <filgum0326@gmail.com>
2025-05-07 15:24:12 +0200

    recursive merge & transfer

---
+++ circuits/build.sh
@@ -0,0 +1,23 @@
+set -e
+
+cd inner
+nargo compile
+bb write_vk --oracle_hash keccak -b ./target/inner.json -o ./target
+cd ..
+
+cd recursive
+nargo compile
+bb write_vk --oracle_hash keccak -b ./target/recursive.json -o ./target
+cd ..
+
+cd transfer
+nargo compile
+bb write_vk --oracle_hash keccak -b ./target/transfer.json -o ./target
+cd ..
+
+cd merge
+nargo compile
+bb write_vk --oracle_hash keccak -b ./target/merge.json -o ./target
+cd ..
+
+echo "Done"
