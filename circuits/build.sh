7de1dd7b08559f2148f9641d815ff68e8a34d7d4
Piotr Stec <piotr.stec2g@gmail.com>
2025-05-13 14:08:38 +0200

    script fixes

--- circuits/build.sh
+++ circuits/build.sh
@@ -2,22 +2,22 @@ set -e
 
 cd inner
 nargo compile
-bb write_vk --oracle_hash keccak -b ./target/inner.json -o ./target
+bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/inner.json -o ./target
 cd ..
 
 cd recursive
 nargo compile
-bb write_vk --oracle_hash keccak -b ./target/recursive.json -o ./target
+bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/recursive.json -o ./target
 cd ..
 
 cd transfer
 nargo compile
-bb write_vk --oracle_hash keccak -b ./target/transfer.json -o ./target
+bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/transfer.json -o ./target
 cd ..
 
 cd merge
 nargo compile
-bb write_vk --oracle_hash keccak -b ./target/merge.json -o ./target
+bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/merge.json -o ./target
 cd ..
 
 echo "Done"
